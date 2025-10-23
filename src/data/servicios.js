ce709101940c4673ceaeec64ac126f065df76100
Damián Parrilli <125889767+damianparrilli@users.noreply.github.com>
2025-10-23 15:40:00 -0300

    Added some animations on Home

--- src/data/servicios.js
+++ src/data/servicios.js
@@ -3,24 +3,80 @@ export const servicios = [
     slug: "consultoria-integral-it",
     title: "Consultoría Integral en IT",
     secciones: [
-      { titulo: "Infraestructura", texto: "Optimización de sistemas, redes y hardware." },
-      { titulo: "Soporte", texto: "Acompañamiento técnico y mantenimiento preventivo." }
-    ]
+      {
+        titulo: "Infraestructura",
+        slug: "infraestructura",
+        texto: "Optimización de sistemas, redes y hardware.",
+        imgDesktop: "",
+        imgMobile: "",
+      },
+      {
+        titulo: "Soporte",
+        slug: "soporte",
+        texto: "Acompañamiento técnico y mantenimiento preventivo.",
+        imgDesktop: "",
+        imgMobile: "",
+      },
+    ],
   },
   {
     slug: "consultoria-en-gestion",
     title: "Consultoría en Gestión",
     secciones: [
-      { titulo: "Comunicación asertiva", texto: "Herramientas prácticas para mejorar tus relaciones." },
-      { titulo: "Motivación y liderazgo", texto: "Fortalecé tu autoconfianza y capacidad de inspirar a otros." }
-    ]
+      {
+        titulo: "Kaizen y 5S: Mejora continua",
+        slug: "kaizen-y-5s",
+        texto:
+          "<b>Qué hacemos:</b><br>•	Implementamos <b>Kaizen</b> para mejorar procesos y fomentar la participación del equipo.<br>•	Aplicamos <b>5S</b> para organizar, estandarizar y mantener el lugar de trabajo seguro y eficiente.<br><br><b>Beneficios clave:</b><br>•	Procesos más ágiles y eficientes.<br>•	Reducción de desperdicios y costos.<br>•	Mayor seguridad y orden en el trabajo.<br>•	Mejor calidad en productos y servicios.<br><br><b>Nuestro servicio incluye:</b><br>•	Diagnóstico inicial.<br>•	Implementación de mejoras y 5S.<br>•	Seguimiento y ajustes continuos.<br><br><b>Convierta su empresa en un espacio más productivo y competitivo.</b>",
+        imgDesktop: "kaizen-desktop",
+        imgMobile: "kaizen-mobile",
+      },
+      {
+        titulo: "Asesoramiento Contable y Administrativo",
+        slug: "asesoramiento-contable-y-administrativo",
+        texto:
+          "Nuestros colaboradores en las distintas áreas podrán evaluar cada uno de los requerimientos y brindarle una solución acorde a su negocio y necesidades de desarrollo, en relación a temas contables y administrativos en general.",
+        imgDesktop: "imagen-contable",
+        imgMobile: "imagen-contable",
+      },
+      {
+        titulo: "Capacitaciones",
+        slug: "capacitaciones",
+        texto:
+          "Con las capacitaciones, se adquieren nuevos conocimientos y especializaciones, logrando un aumento sustancial en la productividad del trabajador.<br><br>\
+<b>1. Habilidades gerenciales:</b> Esta es una de las capacitaciones más importantes, pues tiene por objetivo mejorar el trabajo en equipo.<br><br>\
+<b>2. Gestión de procesos y mejora continua:</b> “Toda organización tiene procesos de operaciones. Entonces tener colaboradores capacitados para incrementar y mejorar los procesos en distintas áreas y organización es fundamental para cualquier organización que quiere prevalecer en el tiempo”.<br><br>\
+<b>3. Seguridad y Salud ocupacional:</b> “Los índices de siniestralidad se han incrementado y ni siquiera en la parte industrial, sino la administrativa. Los colaborares deberían estar capacitados en temas de ergonomía, en oficinas e industriales.<br><br>\
+<b>4. Gestión Ambiental:</b> “Toda empresa impacta en el medio ambiente, por ello la empresa necesita colaboradores especializados en estos temas”.<br><br>\
+<b>5. Continuidad de negocios:</b> Las empresas deben asegurar que sus cadenas de suministro continúen operando bajo cualquier circunstancia.<br><br>\
+<b>6. Coaching:</b> Capacitación aplicada a la optimización del empleado dentro de la empresa.<br><br>\
+<b>7. Kaizen y 5 S:</b> Capacitación en la implementación de esta disciplina en el ámbito administrativo, de gestión e industrial.<br><br>\
+<b>8. Capacitación técnica:</b> Capacitación para los empleados que requieren tener un conocimiento técnico sobre las tecnologías utilizadas en la organización.<br>",
+        imgDesktop: "capacitaciones-imagen",
+        imgMobile: "capacitaciones-imagen",
+      },
+    ],
   },
   {
     slug: "desarrollo-de-estrategias",
     title: "Desarrollo de estrategias",
     secciones: [
-      { titulo: "Comunicación asertiva", texto: "Herramientas prácticas para mejorar tus relaciones." },
-      { titulo: "Motivación y liderazgo", texto: "Fortalecé tu autoconfianza y capacidad de inspirar a otros." }
-    ]
+      {
+        titulo: "Kaizen y 5S: Mejora continua",
+        slug: "kaizen-y-5s-mejora-continua",
+        texto:
+          "<b>Qué hacemos:</b><br><br>•	Implementamos <b>Kaizen</b> para mejorar procesos y fomentar la participación del equipo.<br>•	Aplicamos <b>5S</b> para organizar, estandarizar y mantener el lugar de trabajo seguro y eficiente.<br><br><b>Beneficios clave:</b><br>•	Procesos más ágiles y eficientes.<br>•	Reducción de desperdicios y costos.<br>•	Mayor seguridad y orden en el trabajo.<br>•	Mejor calidad en productos y servicios.<br><br><b>Nuestro servicio incluye:</b><br>•	Diagnóstico inicial.<br>•	Implementación de mejoras y 5S.<br>•	Seguimiento y ajustes continuos.<br><br><b>Convierta su empresa en un espacio más productivo y competitivo.</b>",
+        imgDesktop: "",
+        imgMobile: "",
+      },
+      {
+        titulo: "Coaching Ontológico",
+        slug: "coaching-ontologico",
+        texto:
+          "Disciplina  destinada a descubrir la mejor versión de uno mismo, descubriendo el máximo potencial.<br>Algunos resultados concretos que se pueden obtener en el personal gracias al coaching:<br>– Contexto: La persona comprende de manera ágil, entretenida y profunda, el contexto en el que se está desenvolviendo, y podrá reconocer de qué forma participa en él.<br>– Descubrir: como personas, tenemos muchos “velos” que suelen cubrir nuestras cualidades y también nuestras falencias.<br>– Progreso constante<br>– Comunicación asertiva: lograrán en un corto plazo encontrar las herramientas necesarias y apropiadas para relacionarse en forma más efectiva con todo tipo de personas; y a partir de allí, plantear la posibilidad de seguir creciendo.<br><b>– Motivación y liderazgo</b><br><b>Y muchos otros logros relacionados con esta disciplina.</b>",
+          imgDesktop: "",
+        imgMobile: "",
+      },
+    ],
   },
 ];
